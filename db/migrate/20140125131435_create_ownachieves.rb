1913e82645f3624066528d46554be7d57c4ccc4e
TK <kramatk@gmail.com>
2014-03-03 12:52:07 +0700

    add create file & folder feature

--- db/migrate/20140125131435_create_ownachieves.rb
+++
@@ -1,9 +0,0 @@
-class CreateOwnachieves < ActiveRecord::Migration
-  def change
-    create_table :ownachieves ,:id => false do |t|
-		t.belongs_to :user
-		t.belongs_to :achievement		
-    end
-
-  end
-end
